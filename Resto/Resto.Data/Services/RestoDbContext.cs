585b5c9663238c1d33289c1b9e92988477aad9cb
Ahmed Saad <ahmedsaad17391739@gmail.com>
2020-08-18 11:48:34 -0400

    Entity framework configuered

---
+++ Resto/Resto.Data/Services/RestoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Resto.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resto.Data.Services
+{
+    public class RestoDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants {get; set;}
+    }
+}
---
+++ Resto/Resto.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,42 @@
+﻿using Resto.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resto.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly RestoDbContext db;
+
+        public SqlRestaurantData(RestoDbContext db)
+        {
+            this.db = db;
+        }
+        public void Add(Restaurant rest)
+        {
+            db.Restaurants.Add(rest);
+            db.SaveChanges();
+        }
+
+        public Restaurant get(int id)
+        {
+            return db.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> getAll()
+        {
+            return db.Restaurants;
+        }
+
+        public void Update(Restaurant rest)
+        {
+            var entry = db.Entry(rest);
+            entry.State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
